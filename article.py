8d0fa06488fb4255941ea24901a07ff285117148
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2022-10-25 13:57:43 +0530

    Update article.py

--- article.py
+++ article.py
@@ -1,4 +1,4 @@
-#***************ADD THREE ARTICLES LINK AND CREATE AN ARRAY.
+#***************ADD EXACTLY THREE ARTICLES LINKS FOR EACH TOPIC IN A NEW ARRAY IN THE SAME ORDER OR INDEX.
 
 articles=[["https://en.wikipedia.org/wiki/Metaverse","https://www.techtarget.com/whatis/feature/The-metaverse-explained-Everything-you-need-to-know","https://www.wired.com/story/what-is-the-metaverse/"], 
 ["https://www.singlegrain.com/web3/web-3-0/","https://coinmarketcap.com/alexandria/article/what-is-web-3-0","https://en.wikipedia.org/wiki/Web3"],
