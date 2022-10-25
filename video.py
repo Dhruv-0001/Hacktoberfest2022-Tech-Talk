b50a0a080b8e8e2b7187fbad7226b7df903da04e
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2022-10-25 13:43:35 +0530

    Update video.py

--- video.py
+++ video.py
@@ -1,4 +1,4 @@
-#***************ADD TWO VIDEOS LINK AND CREATE AN ARRAY.
+#***************ADD ONLY TWO VIDEO LINKS FOR EACH TOPIC AND CREATE AN ARRAY.
 
 
 videos=[["https://www.youtube.com/watch?v=0azMpBagjI4","https://www.youtube.com/watch?v=7DEVfUk2zCk"],
