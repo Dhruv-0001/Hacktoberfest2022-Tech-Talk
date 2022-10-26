29c37ed68668e35a02337a235c09269fbc6772d3
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2022-10-26 20:41:19 +0530

    Update article.py

--- article.py
+++ article.py
@@ -1,6 +1,7 @@
 #***************ADD EXACTLY THREE ARTICLES LINKS FOR EACH TOPIC IN A NEW ARRAY IN THE SAME ORDER OR INDEX.
 
 articles=[["https://en.wikipedia.org/wiki/Metaverse","https://www.techtarget.com/whatis/feature/The-metaverse-explained-Everything-you-need-to-know","https://www.wired.com/story/what-is-the-metaverse/"], 
-["https://www.singlegrain.com/web3/web-3-0/","https://coinmarketcap.com/alexandria/article/what-is-web-3-0","https://en.wikipedia.org/wiki/Web3"],["https://academy.binance.com/en/glossary/blockchain?utm_campaign=googleadsxacademy&utm_source=bing_ads&utm_medium=cpc&ref=TDJYLZ1Y","https://www.ibm.com/in-en/topics/what-is-blockchain","https://www.investopedia.com/terms/b/blockchain.asp"
+["https://www.singlegrain.com/web3/web-3-0/","https://coinmarketcap.com/alexandria/article/what-is-web-3-0","https://en.wikipedia.org/wiki/Web3"],
+["https://academy.binance.com/en/glossary/blockchain?utm_campaign=googleadsxacademy&utm_source=bing_ads&utm_medium=cpc&ref=TDJYLZ1Y","https://www.ibm.com/in-en/topics/what-is-blockchain","https://www.investopedia.com/terms/b/blockchain.asp"
 
 ]
