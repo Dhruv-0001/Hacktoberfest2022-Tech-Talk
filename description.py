25e05417e249effc1e26b9cef03f44a9a2120496
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2022-10-27 23:58:24 +0530

    Update description.py

--- description.py
+++ description.py
@@ -3,6 +3,9 @@
 description=["Metaverse is a kind of virtual world. With this technology, people can enter the digital world through virtual identity. In this virtual space, people also get a chance to hang out, shop, and meet friends. Metaverse combines several technologies such as augmented reality, virtual reality, machine learning, blockchain technology, and artificial intelligence.",
 
 "Web 3.0 (also known as web3) is the third iteration or version of the Internet that interconnects data in a decentralized way to deliver a faster and more personalized user experience. It is built using artificial intelligence, machine learning and the semantic web, and uses the blockchain security system to keep your information safe and secure.",
-"A blockchain is the foundation for immutable ledgers, or records of transactions that cannot be altered, deleted, or destroyed. This is why blockchains are also known as a distributed ledger technology (DLT).","Artificial Intelligence (AI) Artificial intelligence leverages computers and machines to mimic the problem-solving and decision-making capabilities of the human mind."
+
+"A blockchain is the foundation for immutable ledgers, or records of transactions that cannot be altered, deleted, or destroyed. This is why blockchains are also known as a distributed ledger technology (DLT).",
+
+"Artificial Intelligence (AI) Artificial intelligence leverages computers and machines to mimic the problem-solving and decision-making capabilities of the human mind.",
 
 ]
