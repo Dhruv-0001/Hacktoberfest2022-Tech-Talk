19753c4598aea6449a0c75e7c015f26682b61893
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2022-10-31 23:39:43 +0530

    Update video.py

--- video.py
+++ video.py
@@ -2,12 +2,10 @@
 
 
 videos=[["https://www.youtube.com/watch?v=0azMpBagjI4","https://www.youtube.com/watch?v=7DEVfUk2zCk"],
-
 ["https://www.youtube.com/watch?v=XiWLLepUL-s","https://www.youtube.com/watch?v=wHTcrmhskto"],
 ["https://www.youtube.com/watch?v=SSo_EIwHSd4","https://www.youtube.com/watch?v=YJyXfjbBmc8"],
 ["https://www.youtube.com/watch?v=GHpchgLoDvI&list=PLp6ek2hDcoNB_YJCruBFjhF79f5ZHyBuz","https://www.youtube.com/watch?v=JMUxmLyrhSk"],
-["https://www.youtube.com/watch?v=LlhmzVL5bm8","https://www.youtube.com/watch?v=h0gWfVCSGQQ"]
-
+["https://www.youtube.com/watch?v=LlhmzVL5bm8","https://www.youtube.com/watch?v=h0gWfVCSGQQ"],
 
 ]
 
